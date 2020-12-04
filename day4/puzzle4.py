c561208a4e2f6cddc856a176bec950ee4c568e16
xanderwessels <x.b.wessels@student.tue.nl>
2020-12-04 18:08:38 +0100

    updated part 1 to actually work

--- day4/puzzle4.py
+++ day4/puzzle4.py
@@ -1,6 +1,6 @@
 import numpy as np
-PROP1 = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"].sort()
-PROP2 = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].sort()
+PROP1 = sorted(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"])
+PROP2 = sorted(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
 
 def read_input():
     counter = 0
@@ -23,9 +23,12 @@ def process_passport(raw_passport):
         properties.append(prop.split(":")[0])
     if len(properties) < 7:
         return 0
-    if len(properties) == 7 and properties.sort() == PROP2:
-        return 1
-    if len(properties) == 8 and properties.sort() == PROP1:
+    if len(properties) == 7:
+        if "cid" not in properties:
+            return 1
+        else:
+            return 0
+    if len(properties) == 8:
         return 1
 
 
